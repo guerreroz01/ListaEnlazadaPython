1875e1509689dca12f4c7392b94f25ae9d6f219f
Oliver Reyes <oliver.dentistry@gmail.com>
2020-11-21 23:56:07 +0100

    append size property and the return value

--- listaEnlazada.py
+++ listaEnlazada.py
@@ -12,6 +12,7 @@ class LinkList:
         self._anchor = None
         self._last = None
         self._work = None
+        self.size = 0
 
     # agregar Nodo
     def appendNode(self, data):
@@ -19,6 +20,8 @@ class LinkList:
             NewNode = Node(data)
             self._anchor = NewNode
             self._last = NewNode
+            self.size += 1
+            return self._last
         else:
             self._work = self._anchor
             while True:
@@ -27,9 +30,11 @@ class LinkList:
                     NewNode.head = self._work
                     self._work.next = NewNode
                     self._last = NewNode
+                    self.size += 1
                     break
                 
                 self._work = self._work.next
+            return self._last
 
     # recorrer lista hacia adelante
     def showListForward(self):
@@ -51,13 +56,17 @@ class LinkList:
     # borrar nodo
     def deleteNode(self, data):
         self._work = self._anchor
+        nodeToDelete = None
         while True:
             if self._work.data == data:
+                nodeToDelete = self._work
                 self._work.head.next = self._work.next
                 self._work.next.head = self._work.head
+                self.size -= 1
                 break
 
             self._work = self._work.next
+        return nodeToDelete
 
 
     # agregar un nodo antes de un dato
@@ -70,9 +79,11 @@ class LinkList:
                 NewNode.head = self._work.head
                 self._work.head.next = NewNode
                 self._work.head = NewNode
+                self.size -= 1
                 break
             
             self._work = self._work.next
+        return self._work.head
 
     # agregar un nodo despues de un dato
     def insertNodeAfter(self, dataAfter, data):
@@ -84,20 +95,24 @@ class LinkList:
                 NewNode.head = self._work
                 NewNode.next.head = NewNode
                 self._work.next = NewNode
+                self.size -= 1
                 break
 
             self._work = self._work.next
+        return self._work.next
 
     # actualizar nodo
     def updateNode(self, oldData, newData):
         self._work = self._anchor
+        updatedNode = None
         while True:
             if self._work.data == oldData:
                 self._work.data = newData
+                updatedNode = self._work
                 break
 
             self._work = self._work.next
-
+        return updatedNode
 
 
 
@@ -112,10 +127,7 @@ miLista.appendNode(500)
 miLista.appendNode(600)
 
 
+x = miLista.insertNodeAfter(500, 50000)
 miLista.showListForward()
-print("\n*********************************\n")
-
-miLista.updateNode(300, 3000)
-
-print("\n*********************************\n")
-miLista.showListForward()
+print("\n******************************\n")
+print(x.data)
